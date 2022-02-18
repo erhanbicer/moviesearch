5a1c2c75c3f9975f0ac908e3f775bbaf989a4945
Erhan BICER <erhan@saveas.com.tr>
2022-02-18 12:33:19 +0300

    feat: anasayfa tamamlandı

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,16 +1,86 @@
 import React, { FC } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  FlatList,
+  ListRenderItem,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import { Loading } from '@components/Loading';
+import R from '@resources';
+import { MovieItem } from '@screens/home/component/MovieItem';
+import { SearchMovie } from '@services/APIService';
+import { useMoviesFetch } from '@hooks/useMoviesFetch';
 
-interface HomeScreenProps {}
+export const HomeScreen: FC = () => {
+  const { movies, searchTerm, loading, errorText, setSearchTerm, fetchMovie } =
+    useMoviesFetch();
 
-export const HomeScreen: FC<HomeScreenProps> = () => {
+  if (movies === undefined) {
+    return <Loading />;
+  }
+
+  const renderItem: ListRenderItem<SearchMovie> = ({ item }) => {
+    return <MovieItem item={item} />;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView style={styles.flex}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInput}
+          placeholder={'Search movie...'}
+          underlineColorAndroid={'transparent'}
+          placeholderTextColor={R.theme.placeholder}
+          value={searchTerm}
+          onChangeText={setSearchTerm}
+          onSubmitEditing={() => fetchMovie(searchTerm)}
+          selectionColor={R.theme.white}
+        />
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            {errorText ? (
+              <Text>{errorText}</Text>
+            ) : (
+              <FlatList
+                style={styles.flex}
+                keyExtractor={(item) => item.imdbID}
+                data={movies}
+                renderItem={renderItem}
+                showsVerticalScrollIndicator={false}
+              />
+            )}
+          </>
+        )}
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center' },
+  flex: { flex: 1 },
+  container: {
+    flex: 1,
+    paddingHorizontal: R.dimen.m_w,
+    backgroundColor: R.theme.primary,
+  },
+  inputContainer: {
+    backgroundColor: R.theme.primary,
+  },
+  textInput: {
+    height: R.dimen.xxl_h,
+    width: '80%',
+    marginVertical: R.dimen.m_h,
+    marginHorizontal: '10%',
+    borderColor: R.theme.white,
+    borderWidth: 0.5,
+    color: R.theme.white,
+    fontSize: R.fontSize.h3,
+    paddingLeft: R.dimen.xs_w,
+    borderRadius: R.dimen.xs_h,
+  },
 });
---
+++ src/screens/home/component/MovieItem.tsx
@@ -0,0 +1,69 @@
+import React, { FC, useCallback } from 'react';
+import { Pressable, StyleSheet, Text } from 'react-native';
+import { SearchMovie } from '@services/APIService/DTOs/SearchMovie';
+import FastImage from 'react-native-fast-image';
+import R from '@resources';
+import { useNavigation } from '@react-navigation/native';
+import Animated from 'react-native-reanimated';
+import { VibrancyView } from '@react-native-community/blur';
+import { useOpacityAnimation } from '@hooks/animation';
+
+interface MovieItemProps {
+  item: SearchMovie;
+}
+
+const AnimatedBlurView = Animated.createAnimatedComponent(VibrancyView);
+
+export const MovieItem: FC<MovieItemProps> = ({ item }) => {
+  const navigation = useNavigation();
+  const { opacityValue, opacityStyle } = useOpacityAnimation();
+
+  const handleButton = useCallback(() => {
+    //todo: navigation tipi kontrol edilecek
+    // @ts-ignore
+    navigation.navigate('DetailScreen', { imdbID: item.imdbID });
+  }, [item.imdbID, navigation]);
+
+  return (
+    <Pressable style={styles.container} onPress={handleButton}>
+      <FastImage
+        style={styles.image}
+        resizeMode={FastImage.resizeMode.contain}
+        onLoadStart={() => (opacityValue.value = 0)}
+        source={{ uri: item.poster }}>
+        <AnimatedBlurView
+          style={[StyleSheet.absoluteFillObject, styles.zIndex, opacityStyle]}
+          blurAmount={12}
+        />
+      </FastImage>
+      <Text style={styles.text}>{`${item.title} (${item.year})`}</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: R.theme.black,
+    flexDirection: 'row',
+    borderRadius: R.dimen.m_h,
+    paddingLeft: R.dimen.m_w,
+    marginTop: R.dimen.xl_h,
+  },
+  text: {
+    flex: 1,
+    marginHorizontal: R.dimen.m_w,
+    alignSelf: 'center',
+    color: R.theme.white,
+  },
+  image: {
+    width: R.dimen.xxl_w * 2.5,
+    borderRadius: R.dimen.m_h,
+    height: R.dimen.xxl_h * 3,
+    justifyContent: 'flex-end',
+    bottom: R.dimen.m_h,
+    overflow: 'hidden',
+  },
+  zIndex: {
+    zIndex: 1,
+  },
+});
