7fe4f0d8d74567d77681b8bbac805da566910aba
Erhan BICER <erhan@saveas.com.tr>
2022-02-18 12:24:31 +0300

    feat: analytics initialize

--- src/components/Initializer.tsx
+++ src/components/Initializer.tsx
@@ -1,34 +1,50 @@
-import React, { FC } from 'react';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
-import commonStyles from '@resources/commonStyles';
+import React, { FC, useCallback, useEffect } from 'react';
+import { StatusBar, StyleSheet, View } from 'react-native';
 import { useTypedSelector } from '@hooks/useTypedSelector';
 import { useInternetReachable } from '@hooks/useInternetReachable';
 import { useRemoteConfig } from '@hooks/useRemoteConfig';
 import { APIService } from '@services/APIService/APIService';
+import { Loading } from '@components/Loading';
+import R from '@resources';
+import { Analytics } from '@services/Analytics';
+import { Text } from '@components';
 
 interface InitializerProps {}
 
 export const Initializer: FC<InitializerProps> = ({ children }) => {
+  StatusBar.setBarStyle('light-content');
   const appReady = useTypedSelector((state) => state.appReadiness.appReady);
   const isInternetReachable = useInternetReachable();
   const remoteConfig = useRemoteConfig();
 
-  if (appReady) {
+  const servicesInitiliaze = useCallback(async () => {
     APIService.initialize(remoteConfig);
+    await Analytics.initialize();
+  }, [remoteConfig]);
+
+  useEffect(() => {
+    (async () => {
+      if (appReady) {
+        await servicesInitiliaze();
+      }
+    })();
+  }, [appReady, servicesInitiliaze]);
+
+  if (appReady) {
     return <>{children}</>;
   }
 
   return (
-    <View style={[styles.container, commonStyles.center]}>
+    <View style={[styles.container, R.commonStyles.center]}>
       {isInternetReachable === false ? (
         <Text>İnternet bağlantınızı kontrol ediniz</Text>
       ) : (
-        <ActivityIndicator size={'small'} />
+        <Loading />
       )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
+  container: { flex: 1, backgroundColor: R.theme.primary },
 });
