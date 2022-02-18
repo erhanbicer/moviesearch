45b347888bd171f01e63843e7b7f320c836ab28d
Erhan BICER <erhan@saveas.com.tr>
2022-02-18 12:38:31 +0300

    feat: splashscreen'de ekran ortası yazıya animasyon eklendi

--- src/screens/splash/SplashScreen.tsx
+++ src/screens/splash/SplashScreen.tsx
@@ -1,26 +1,31 @@
 import React, { FC, useEffect, useMemo } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { RemoteConfig } from '@services/RemoteConfig';
 import commonStyles from '@resources/commonStyles';
 import { sleep } from '@utils/sleep';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '@router/AppStackNavigator';
+import { Text } from '@components/Text/Text';
+import { useSplashTextAnimation } from '@hooks/animation';
 
 interface SplashScreenProps
   extends StackScreenProps<RootStackParamList, 'SplashScreen'> {}
 
 export const SplashScreen: FC<SplashScreenProps> = (props) => {
+  const { textAnimatedStyle } = useSplashTextAnimation();
+
   const companyName = useMemo(
     () => RemoteConfig.getInstance().getStringValue('COMPANY_NAME'),
     [],
   );
+
   useEffect(() => {
     sleep(3000).then(() => props.navigation.replace('HomeScreen'));
   }, [props.navigation]);
 
   return (
     <View style={[styles.container, commonStyles.center]}>
-      <Text>{companyName}</Text>
+      <Text animated={textAnimatedStyle}>{companyName}</Text>
     </View>
   );
 };
